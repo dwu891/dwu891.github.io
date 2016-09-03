938fde71cd839a0e56fe7a6f470c67393c29e3a4
Daria Wu <wu_daria@yahoo.com>
2016-09-02 21:19:10 -0700

    Updated jquery to hide menu when scrolling

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ $('#menu-btn').click(function() {
    $('#mobile-nav').slideToggle(500); 
 });
 
+$(window).scroll(function() {
+    if($(window).scrollTop() > 0 && $('#mobile-nav').is(":visible"))
+    {
+        $('#mobile-nav').hide(200);
+    }
+});
+
 if($(window).width() > 600) {
    $('#mobile-nav').hide();
 } 
